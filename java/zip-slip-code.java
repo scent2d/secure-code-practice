1ac89c01630bee202a751028554cdbc44f7e8e84
scent2d <45614318+scent2d@users.noreply.github.com>
2022-01-24 17:37:33 +0900

    Update zip-slip-code.java

--- java/zip-slip-code.java
+++ java/zip-slip-code.java
@@ -14,6 +14,7 @@ public static List<String> zipSlipNoncompliant(ZipFile zipFile) throws IOExcepti
 }
 
 // Compliant Solution
+// 정확한 targetDirectory를 입력받은 후, 입력받은 값과 Extract된 파일의 경로 값과 동일한지 검증하는 로직을 추가해야 한다.
 public static List<String> zipSlipCompliant(ZipFile zipFile, String targetDirectory) throws IOException {
   Enumeration<? extends ZipEntry> entries = zipFile.entries();
   List<String> filesContent = new ArrayList<>();
@@ -23,6 +24,7 @@ public static List<String> zipSlipCompliant(ZipFile zipFile, String targetDirect
     File file = new File(entry.getName());
     String canonicalDestinationPath = file.getCanonicalPath();
 
+    // Extract된 파일의 경로 값이 입력받은 targetDirectory 값과 다를 경우 예외처리한다. 
     if (!canonicalDestinationPath.startsWith(targetDirectory)) {
       throw new IOException("Entry is outside of the target directory");
     }
