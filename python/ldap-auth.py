120f2d78c8595541d271555b8d03f2b2a2e317cc
scent2d <45614318+scent2d@users.noreply.github.com>
2022-03-16 16:21:36 +0900

    commit

---
+++ python/ldap-auth.py
@@ -0,0 +1,35 @@
+'''
+취약점 개요
+LDAP 클라이언트는 무엇보다도 간단한 인증 방법 을 제공하는 "바인드 요청"으로 LDAP 서버에 인증합니다 .
+
+LDAP의 단순 인증은 세 가지 다른 메커니즘과 함께 사용할 수 있습니다.
+
+길이가 0인 사용자 이름과 비밀번호 값으로 바인드 요청을 수행하여 익명 인증 메커니즘 .
+길이가 0인 암호 값으로 바인드 요청을 수행하여 인증되지 않은 인증 메커니즘 입니다.
+길이가 0이 아닌 비밀번호 값으로 바인드 요청을 수행하여 이름/비밀번호 인증 메커니즘 .
+익명 바인드 및 인증되지 않은 바인드를 사용하면 비밀번호를 제공하지 않고도 LDAP 디렉토리의 정보에 액세스할 수 있으므로 사용을 강력히 권장하지 않습니다.
+'''
+
+# Noncompliant Code Example
+import ldap
+
+def init_ldap():
+   connect = ldap.initialize('ldap://example:1389')
+
+   connect.simple_bind('cn=root') # Noncompliant
+   connect.simple_bind_s('cn=root') # Noncompliant
+   connect.bind_s('cn=root', None) # Noncompliant
+   connect.bind('cn=root', None) # Noncompliant
+
+
+# Compliant Solution
+import ldap
+import os
+
+def init_ldap():
+   connect = ldap.initialize('ldap://example:1389')
+
+   connect.simple_bind('cn=root', os.environ.get('LDAP_PASSWORD')) # Compliant
+   connect.simple_bind_s('cn=root', os.environ.get('LDAP_PASSWORD')) # Compliant
+   connect.bind_s('cn=root', os.environ.get('LDAP_PASSWORD')) # Compliant
+   connect.bind('cn=root', os.environ.get('LDAP_PASSWORD')) # Compliant
